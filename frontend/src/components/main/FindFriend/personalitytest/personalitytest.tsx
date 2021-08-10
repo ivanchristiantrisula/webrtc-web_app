4edf4b9c84289aa0dc2171bb8a8e8b970eb60d33
Ivan Christian <ivanchristian8a@gmail.com>
2021-08-10 10:11:08 +0700

    progress personalitytest

--- frontend/src/components/main/FindFriend/personalitytest/personalitytest.tsx
+++ frontend/src/components/main/FindFriend/personalitytest/personalitytest.tsx
@@ -5,15 +5,39 @@ import {
   makeStyles,
   Paper,
   Theme,
+  Button,
 } from "@material-ui/core";
 import { useEffect, useState } from "react";
+import LinearProgress, {
+  LinearProgressProps,
+} from "@material-ui/core/LinearProgress";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox, { CheckboxProps } from "@material-ui/core/Checkbox";
 import "./style.css";
+
+function LinearProgressWithLabel(
+  props: LinearProgressProps & { value: number }
+) {
+  return (
+    <Box display="flex" alignItems="center">
+      <Box width="100%" mr={1}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box minWidth={35}>
+        <Typography variant="body2" color="textSecondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     display: "flex",
-    alignContent: "center",
-    justifyContent: "center",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+    alignContent: "stretch",
     overflow: "hidden",
     width: "100%",
     height: "100%",
@@ -30,7 +54,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     paddingBottom: "20px",
   },
   answersArea: {
-    height: "50%",
+    height: "40%",
     width: "100%",
     display: "flex",
     justifyContent: "center",
@@ -46,10 +70,24 @@ const useStyles = makeStyles((theme: Theme) => ({
   progressContainer: {
     width: "100%",
     height: "10%",
+    paddingTop: "5%",
   },
   navigationContainer: {
     width: "100%",
-    height: "10%",
+    height: "5%",
+
+    display: "flex",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    width: "50%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+  navButton: {
+    width: "50%",
+    height: "100%",
   },
 }));
 
@@ -59,24 +97,88 @@ export default () => {
     require("./questions.json").questions
   );
   const [answers, setAnswers] = useState([]);
-  const [questionNum, setQuestionNum] = useState(0);
+  const [currQuestionNum, setCurrQuestionNum] = useState(0);
+
+  useEffect(() => {
+    console.log(answers);
+  }, [answers]);
+
+  const nextQuestion = () => {
+    setCurrQuestionNum(currQuestionNum + 1);
+  };
+
+  const prevQuestion = () => {
+    setCurrQuestionNum(currQuestionNum - 1);
+  };
+
+  const pickSelection = (selection: number) => {
+    if (answers[currQuestionNum] === undefined) {
+      setAnswers((old) => [...old, selection]);
+    } else {
+      let x = answers;
+      x[currQuestionNum] = selection;
+      setAnswers([...x]);
+    }
+  };
 
   return (
     <Container className={classes.root}>
-      <Box className={classes.progressContainer}></Box>
+      <Box className={classes.progressContainer}>
+        <LinearProgressWithLabel
+          value={(answers.length / (questions.length - 1)) * 100}
+        />
+      </Box>
       <Box className={classes.questionContainer}>
-        <Typography variant="h4">{questions[questionNum].title}</Typography>
+        <Typography variant="h4">{questions[currQuestionNum].title}</Typography>
       </Box>
       <Box className={classes.answersArea}>
-        {questions[questionNum].selections.map((selection: string) => {
-          return (
-            <Paper elevation={5} className={classes.answerBox}>
-              <Typography variant="subtitle1">{selection}</Typography>
-            </Paper>
-          );
-        })}
+        {questions[currQuestionNum].selections.map(
+          (selection: string, idx: number) => {
+            return (
+              <Paper
+                elevation={5}
+                className={classes.answerBox}
+                onClick={() => pickSelection(idx)}
+                color={answers[currQuestionNum] === idx ? "red" : "black"}
+              >
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={answers[currQuestionNum] == idx ? true : false}
+                    />
+                  }
+                  label={
+                    <Typography variant="subtitle1">{selection}</Typography>
+                  }
+                />
+              </Paper>
+            );
+          }
+        )}
       </Box>
-      <Box className={classes.navigationContainer}></Box>
+      <Box className={classes.navigationContainer}>
+        <Box className={classes.buttonContainer}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.navButton}
+            onClick={prevQuestion}
+          >
+            Previous Question
+          </Button>
+        </Box>
+        <Box className={classes.buttonContainer}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.navButton}
+            onClick={nextQuestion}
+            disabled={answers[currQuestionNum] === undefined}
+          >
+            Next Question
+          </Button>
+        </Box>
+      </Box>
     </Container>
   );
 };
